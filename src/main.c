6a020312265e190a3d1e18ccdccc635fa77c14a6
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-04-16 23:52:00 -0300

    Ignored even numbers

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
 #include <stdlib.h>
 
 #define COMPOSITE 1
+#define PRIME 0
 
 int main(int argc, char *argv[]) {
   char *sieve;
+  unsigned long sieve_size;
   unsigned long upper_limit;
   unsigned long primes;
 
   char *endptr;
-  unsigned long i, j;
-  char first;
+  unsigned long i, j, k, acc;
+  int mul;
 
   upper_limit = strtoul(argv[1], &endptr, 10);
 
@@ -23,54 +25,38 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  printf("Create a list of consecutive integers from 2 to %lu\n", upper_limit);
-  sieve = calloc(upper_limit + 1, sizeof(char));
+  sieve_size = (upper_limit - 1) / 2 + 1;
+  sieve = malloc((sieve_size + 1) * sizeof(char));
+
   sieve[0] = COMPOSITE;
-  sieve[1] = COMPOSITE;
 
-  primes = upper_limit - 1;
-  printf("There are %lu numbers in this list\n", primes);
+  for (i = 1; i < sieve_size; i++)
+    sieve[i] = PRIME;
 
-  for (i = 2; i * i <= upper_limit; i++) {
+  primes = sieve_size;
+
+  acc = 0;
+  for (i = 1, k = 3; k * k <= upper_limit; i++, k += 2) {
+    acc += k;
+    if (k == 5)
+      mul = 2;
+    else
+      mul = 1;
     if (!sieve[i]) {
-      printf("Number %lu is marked as prime\n", i);
-      first = 1;
-
-      for (j = i * i; j <= upper_limit; j += i) {
+      for (j = i * i + acc; j < sieve_size; j += k * mul) {
         if (!sieve[j]) {
-          if (first) {
-            printf("Mark %lu", j);
-            first = 0;
-          } else {
-            printf(", %lu", j);
-          }
-
+          if (k == 5 && mul == 1)
+            mul = 2;
+          else
+            mul = 1;
           sieve[j] = COMPOSITE;
           primes--;
         }
-      }
-
-      if (!first) {
-        printf(" as composite\n");
       }
     }
   }
 
   printf("There are %lu numbers marked as prime\n", primes);
-
-  first = 1;
-  for (i = 2; i <= upper_limit; i++) {
-    if (!sieve[i]) {
-      if (first) {
-        printf("%lu", i);
-        first = 0;
-      } else {
-        printf(", %lu", i);
-      }
-    }
-  }
-
-  printf(" are the prime numbers between 2 and %lu\n", upper_limit);
 
   free(sieve);
   return 0;
