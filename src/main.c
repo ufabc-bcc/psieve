51a1890e2c03aba314ecd97cd350b03ea70b0edd
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-04-20 04:52:23 -0300

    Better performance but some problems

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,13 +16,25 @@
 #define SV_CLR_BIT(A, i) (CLR_BIT(SV_BLK_T, SV_BLK_SZ, SV_BLK_SZ_LG, A, i))
 #define SV_TST_BIT(A, i) (TST_BIT(SV_BLK_T, SV_BLK_SZ, SV_BLK_SZ_LG, A, i))
 
+unsigned long sqrtul(unsigned long x) {
+  unsigned long i;
+
+  if (x == 0 || x == 1)
+    return x;
+
+  i = 2;
+  while (i * i <= x)
+    i++;
+
+  return i - 1;
+}
+
 int main(int argc, char *argv[]) {
   SV_BLK_T *sieve;
   char *endptr;
   int right, sign;
-  unsigned long upper_limit, sieve_size, primes_count;
+  unsigned long upper_limit, sieve_size, blocks, offset, primes_count;
   unsigned long i, j, k, n, aux;
-  unsigned long s, m;
 
   upper_limit = strtoul(argv[1], &endptr, 10);
 
@@ -34,18 +47,17 @@ int main(int argc, char *argv[]) {
   }
 
   sieve_size = (upper_limit + 1) / 6 * 2;
-
   if (sieve_size / 2 * 6 > upper_limit)
     sieve_size--;
 
   sieve = malloc((sieve_size / SV_BLK_SZ + 1) * sizeof(SV_BLK_T));
-
   for (i = 0; i < sieve_size; i++)
     SV_CLR_BIT(sieve, i);
 
+  blocks = sqrtul(upper_limit);
   primes_count = sieve_size + 2;
 
-  for (s = 0; s <= sieve_size; s += 100000000) {
+  for (offset = 0; offset < sieve_size; offset += blocks) {
     for (i = 0, k = 1, n = 5, right = 0; n * n <= upper_limit;
          i++, k += 1 * right, n += 2 + 2 * right, right = !right) {
       if (!SV_TST_BIT(sieve, i)) {
@@ -56,13 +68,11 @@ int main(int argc, char *argv[]) {
           j = (k + n * k) * 2 - 1;
           sign = 1;
         }
-        m = 0;
-        if (s > j) {
-          m = (s - j) / (2 * n);
-        }
-        j += 2 * n * m;
-        // printf("for n = %lu starting j is %lu (s = %lu, m = %lu)\n", n, j, s, m);
-        for (; j <= s + 100000000 && j < sieve_size; j += 2 * n) {
+
+        if (offset > j)
+          j += 2 * n * ((offset - j) / (2 * n));
+
+        for (; j < offset + blocks && j < sieve_size; j += 2 * n) {
           if (!SV_TST_BIT(sieve, j)) {
             SV_SET_BIT(sieve, j);
             primes_count--;
@@ -78,12 +88,6 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-
-/*
-  for (i = 0, n = 5, right = 0; i < sieve_size;
-       i++, n += 2 + 2 * right, right = !right)
-    if (!SV_TST_BIT(sieve, i))
-      printf("S[%lu] is %lu\n", i, n);*/
 
   printf("%lu primes\n", primes_count);
 
