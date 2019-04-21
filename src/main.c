ef84d101ae857f455fc9817215289e87b06a6abd
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-04-21 04:37:26 -0300

    Damn good version

--- src/main.c
+++ src/main.c
@@ -36,7 +36,7 @@ uint64_t sieveSize(uint64_t upper_limit) {
 
   k = (upper_limit + 1) / 6;
   sieve_size = k * 2;
-  if (upper_limit < k * 6)
+  if (upper_limit <= k * 6)
     sieve_size--;
 
   return sieve_size;
@@ -46,12 +46,11 @@ uint64_t sieveIndexOf(uint64_t value) { return sieveSize(value) - 1; }
 
 uint64_t sieveMark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t sieve_base,
                    uint64_t start, uint64_t end) {
-  int8_t right, sign;
-  uint64_t i, j, k, n, aux;
   uint64_t marked;
+  uint64_t i, j, k, n;
+  int8_t right, sign;
 
   marked = 0;
-
   for (i = 0, k = 1, n = 5, right = 0; i <= sieve_base;
        i++, k += 1 * right, n += 2 + 2 * right, right = !right) {
     if (!SV_TST_BIT(sieve, i)) {
@@ -66,12 +65,14 @@ uint64_t sieveMark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t sieve_base,
       if (start > j)
         j += 2 * n * ((start - j) / (2 * n));
 
-      for (; j <= end && j < sieve_size; j += n + 2 * sign * k, sign *= -1)
-        if (j > start && j <= end && j < sieve_size)
+      for (; j <= end && j < sieve_size; j += n + 2 * sign * k, sign *= -1) {
+        if (j > start) {
           if (!SV_TST_BIT(sieve, j)) {
             SV_SET_BIT(sieve, j);
             marked++;
           }
+        }
+      }
     }
   }
 
@@ -81,10 +82,8 @@ uint64_t sieveMark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t sieve_base,
 int main(int argc, char *argv[]) {
   SV_BLK_T *sieve;
   uint64_t sieve_size, sieve_base;
-
   uint64_t upper_limit;
   char *endptr;
-
   uint64_t i, start, end, primes_count;
 
   upper_limit = strtoul(argv[1], &endptr, 10);
