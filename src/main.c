da030f1b4458d33396ea6a90a585b90e9ab9a946
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-04-21 03:51:42 -0300

    Prior to 228 bug

--- src/main.c
+++ src/main.c
@@ -30,23 +30,37 @@ uint64_t sqrtul(uint64_t x) {
   return i - 1;
 }
 
-uint64_t mark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t until,
-              uint64_t upper_limit, uint64_t start, uint64_t end) {
+uint64_t sieveSize(uint64_t upper_limit) {
+  uint64_t sieve_size;
+  uint64_t k;
+
+  k = (upper_limit + 1) / 6;
+  sieve_size = k * 2;
+  if (upper_limit < k * 6)
+    sieve_size--;
+
+  return sieve_size;
+}
+
+uint64_t sieveIndexOf(uint64_t value) { return sieveSize(value) - 1; }
+
+uint64_t sieveMark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t sieve_base,
+                   uint64_t start, uint64_t end) {
   int8_t right, sign;
   uint64_t i, j, k, n, aux;
   uint64_t marked;
 
   marked = 0;
 
-  for (i = 0, k = 1, n = 5, right = 0; i <= until;
+  for (i = 0, k = 1, n = 5, right = 0; i <= sieve_base;
        i++, k += 1 * right, n += 2 + 2 * right, right = !right) {
     if (!SV_TST_BIT(sieve, i)) {
       if (!right) {
-        j = (k * n - k) * 2 - 1;
         sign = -1;
+        j = (k * n - k) * 2 - 1;
       } else {
-        j = (k + n * k) * 2 - 1;
         sign = 1;
+        j = (k + n * k) * 2 - 1;
       }
 
       if (start > j)
@@ -66,11 +80,12 @@ uint64_t mark(SV_BLK_T *sieve, uint64_t sieve_size, uint64_t until,
 
 int main(int argc, char *argv[]) {
   SV_BLK_T *sieve;
-  uint64_t upper_limit, upper_limit_sqrt, upper_limit_sqrt_idx;
-  uint64_t sieve_size;
-  uint64_t start, end, primes_count;
-  uint64_t i, k;
+  uint64_t sieve_size, sieve_base;
+
+  uint64_t upper_limit;
   char *endptr;
+
+  uint64_t i, start, end, primes_count;
 
   upper_limit = strtoul(argv[1], &endptr, 10);
 
@@ -82,16 +97,8 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  k = (upper_limit + 1) / 6;
-  sieve_size = k * 2;
-  if (upper_limit < k * 6)
-    sieve_size--;
-
-  upper_limit_sqrt = sqrtul(upper_limit);
-  k = (upper_limit_sqrt + 1) / 6;
-  upper_limit_sqrt_idx = k * 2;
-  if (upper_limit_sqrt < k * 6)
-    upper_limit_sqrt_idx--;
+  sieve_size = sieveSize(upper_limit);
+  sieve_base = sieveIndexOf(sqrtul(upper_limit));
 
   sieve = malloc((sieve_size / SV_BLK_SZ + 1) * sizeof(SV_BLK_T));
   for (i = 0; i < sieve_size; i++)
@@ -100,8 +107,7 @@ int main(int argc, char *argv[]) {
   primes_count = sieve_size + 2;
   for (start = 0, end = SV_BLK_CT; start < sieve_size;
        start = end, end += SV_BLK_CT) {
-    primes_count -=
-        mark(sieve, sieve_size, upper_limit_sqrt_idx, upper_limit, start, end);
+    primes_count -= sieveMark(sieve, sieve_size, sieve_base, start, end);
   }
 
   printf("%lu primes\n", primes_count);
